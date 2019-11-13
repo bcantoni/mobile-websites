128a84fea2f0ed738fc3bd3b9459292f0e0ebc60
Brian Cantoni <brian@cantoni.org>
2019-11-13 08:46:55 -0800

    Add Favicons from favicon.io

--- src/index.php
+++ src/index.php
@@ -47,6 +47,10 @@
 <meta name="description" content="<?php echo $desc; ?>">
 <meta name="keywords" content="mobile,mobile web,websites,android,iphone,blackberry,windows,smartphone">
 <meta name=viewport content="width=device-width, initial-scale=1">
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">
 <style type="text/css">
  body {color:#000000; background-color:#ffffff;}
  h1,h2,h3,h4,p,li {font-family:sans-serif;}
